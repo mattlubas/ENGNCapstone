adf70c8b294daef5df4d60dc542e5ceaee1411e5
rwagajua18 <32525955+rwagajua18@users.noreply.github.com>
2018-03-28 18:48:22 -0400

    Add files via upload
    This codes directly communicates with the arduino. The data is automatically sent to excel and also the user can directly input tidal volume and frequency from the python code.

---
+++ ardunio_excel.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+'''
+This code exports data from arduino to excel and allows user input of tidal volume and frequency
+Authors: Alfred Rwagaju, Professor Simon Levy, Matt Lubas, Eric Wirth
+'''
+
+ARDUINO = '/dev/cu.usbmodem1365201'
+BAUD    = 57600
+
+from threading import Thread
+from time import time
+from datetime import datetime
+from serial import Serial
+
+def writefile(file, arduino):
+
+    while True:
+
+        try:
+
+            c = arduino.read().decode('utf8')
+
+            file.write(c)
+
+        except:
+
+            return
+
+def error():
+    
+    print('Please input two legitimate floating-point values')
+
+if __name__ == '__main__':
+
+    arduino = Serial(ARDUINO, BAUD)
+
+    filename = datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H-%M-%S') + '.csv'
+
+    print('Writing to ' + filename)
+
+    file = open(filename, 'w')
+    
+    thread = Thread(target=writefile, args = (file, arduino))
+    thread.daemon = True
+    thread.start()
+
+    while True:
+
+        response = input('Enter new tidal volume and frequency (q to quit) > ')
+
+        if response[0].lower() == 'q':
+            break
+
+        values = response.split()
+
+        okay = False
+
+        if len(values) == 2:                # did we get two values:
+            try:
+                [float(x) for x in values]  # are they floating-point numbers?
+                okay = True
+            except:
+                error()
+        else:
+            error()
+            
+        if okay:
+
+            print('Sending: ' + response)
+            arduino.write(response.encode('utf8'))
+
+    arduino.close()
+    
+    file.close()
+
+
